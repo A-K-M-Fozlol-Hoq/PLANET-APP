916933c52420215bd3580532d5febb0253c3d2b7
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-06-22 02:48:58 +0600

    collected planets data and showig then at hoem screen

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,13 +1,27 @@
-import { View, SafeAreaView, StyleSheet } from 'react-native';
+import { View, FlatList, SafeAreaView, StyleSheet } from 'react-native';
 import React from 'react';
 import Text from '../components/text/text';
 import PlanetHeader from '../components/planet-header';
 import { colors } from '../theme/colors';
+import { PLANET_LIST } from '../data/planet-list';
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader />
+      <FlatList
+        data={PLANET_LIST}
+        keyExtractor={(item) => {
+          return item.name;
+        }}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text>{item.name}</Text>
+            </View>
+          );
+        }}
+      ></FlatList>
     </SafeAreaView>
   );
 }
