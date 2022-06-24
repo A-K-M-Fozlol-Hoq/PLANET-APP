540a1d29fae83aa01f3c645c60f7b698c9fdaf52
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-06-24 18:35:51 +0600

    implemented search option after completing the project :p

--- src/screens/home.js
+++ src/screens/home.js
@@ -4,8 +4,9 @@ import {
   SafeAreaView,
   StyleSheet,
   Pressable,
+  TextInput,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import Text from '../components/text/text';
 import PlanetHeader from '../components/planet-header';
 import { colors } from '../theme/colors';
@@ -36,16 +37,36 @@ const PlanetItem = ({ item }) => {
 };
 
 export default function Home({ navigation }) {
+  const [list, setList] = useState(PLANET_LIST);
   const renderItem = ({ item }) => {
     return <PlanetItem item={item} />;
+  };
+
+  const searchFilter = (text) => {
+    const filteredList = PLANET_LIST.filter((item) => {
+      const itemName = item.name.toLowerCase();
+      const userTypedText = text.toLowerCase();
+
+      return itemName.indexOf(userTypedText) > -1;
+    });
+    setList(filteredList);
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader />
+      <TextInput
+        placeholder="Type the planet name."
+        placeholderTextColor={colors.white}
+        autoCorrect={false}
+        style={styles.searchInput}
+        onChangeText={(text) => {
+          searchFilter(text);
+        }}
+      ></TextInput>
       <FlatList
         contentContainerStyle={styles.list}
-        data={PLANET_LIST}
+        data={list}
         keyExtractor={(item) => {
           return item.name;
         }}
@@ -82,5 +103,12 @@ const styles = StyleSheet.create({
   separator: {
     borderBottomColor: colors.white,
     borderWidth: 0.5,
+  },
+  searchInput: {
+    padding: spacing[4],
+    color: colors.white,
+    borderBottomColor: colors.white,
+    borderWidth: 1,
+    margin: spacing[5],
   },
 });
